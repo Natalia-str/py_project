1bb0164488fac2be1ee0c79f3ecae3fdb0c32311
Natalia Stroganova <ntstroganova@gmail.com>
2023-03-26 22:42:17 +0300

    Add subtraction function

--- funcs.py
+++ funcs.py
@@ -2,3 +2,9 @@ def get_sum(a=0, b=0):
     """складывает 2 числа"""
     sum_result = a + b
     return sum_result
+
+
+def get_subtr(a=0, b=0):
+    """вычитает из первого числа второе"""
+    subtr_result = a - b
+    return subtr_result
